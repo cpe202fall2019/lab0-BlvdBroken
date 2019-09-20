f16633dc29b5a6e2ffeb0b58901facdf68791f2b
Sam <skazimir@calpoly.edu>
2019-09-20 14:58:24 -0700

    Update planets.py

--- planets.py
+++ planets.py
@@ -1,6 +1,10 @@
+from pip._vendor.distlib.compat import raw_input
+
+
 def weight_on_planets():
-   weight = raw_input ("What do you weigh on earth? ")
-   print ("On Mars you would weigh ", (weight * 0.38), " pounds.\nOn Jupiter you would weigh ", (weight * 2.38), " pounds.")
-   
+    weight = float(raw_input("What do you weigh on earth? "))
+    print("On Mars you would weigh ", (weight * 0.38), " pounds.\nOn Jupiter you would weigh ", (weight * 2.38), "pounds.")
+
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
