af3e2656b2d0e3d4044fcd67dc41036ebbe79ae1
Sam <skazimir@calpoly.edu>
2019-09-24 23:58:34 -0700

    Final 2

--- planets.py
+++ planets.py
@@ -1,8 +1,5 @@
-from pip._vendor.distlib.compat import raw_input
-
-
 def weight_on_planets():
-    weight = float(raw_input("What do you weigh on earth? "))
+    weight = float(input("What do you weigh on earth? "))
     print("\nOn Mars you would weigh", (weight * 0.38), "pounds.\nOn Jupiter you would weigh", (weight * 2.34), "pounds.")
 
 
--- planets_tests.py
+++ planets_tests.py
@@ -7,12 +7,12 @@ class Test_planets(unittest.TestCase):
 
     def test_01(self):
         sys.stdin = io.StringIO("136")
-        sys.stdout = student_output = io.StringIO()        
+        sys.stdout = student_output = io.StringIO()
         expected_out = "What do you weigh on earth? \nOn Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds."
         weight_on_planets()
-        print(student_output.getvalue().strip())
-        print(expected_out.strip())
-        self.assertEqual(expected_out, student_output.getvalue().strip())
+        #print(student_output.getvalue().strip())
+        #print(expected_out.strip())
+        self.assertEqual(expected_out.strip(), student_output.getvalue().strip())
 
 
 if __name__ == "__main__":
